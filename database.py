c8c323f2553a429001168262a2d508d898657245
Bryan Jones <bryanjones@Bryans-MBP.attlocal.net>
2024-08-06 18:17:04 -0500

    Update database configuration to use environment variable

--- database.py
+++ database.py
@@ -2,17 +2,13 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
-
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
-
-# PostgreSQL Series
+# Fetching the database URL from environment variables
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
